dd1f8e825f1ed52860389d12cb1084a0800a2f16
ViceElez <vice.elezovic12@gmail.com>
2024-11-10 16:59:06 +0100

    zavrsen izbornik za korisnika

--- upravljanje financijama/upravljanje financijama/Program.cs
+++ upravljanje financijama/upravljanje financijama/Program.cs
@@ -16,7 +16,7 @@ class Program
                 case 1:
                     {
                         Console.Clear();
-                        Console.WriteLine("kor");
+                        UserMenu();
                         break;
                     }
 
@@ -32,13 +32,180 @@ class Program
                         break;
                     }
                 default:
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Krivi unos, molimo pokusajte ponovo");
-                        break;
+                    { 
+                     Console.WriteLine("Krivi unos, molimo pokusajte ponovo");
+                     Console.ReadKey();
+                     Console.Clear();    
+                     break;
                     }
             }
         }
 
     }
+
+    static void UserMenu()
+    {
+        bool userMenuRunning = true;
+        while (userMenuRunning)
+        {
+            Console.Clear();
+            Console.WriteLine("1 - Unos novog korisnika\n2 - Brisanje korisnika\n3 - Uredivanje korisnika\n4 - Pregled korisnika\n5 - Vracanje na glavni meni");
+            var isInputCorrect = int.TryParse(Console.ReadLine(), out var inputForUserMenu);
+            switch (inputForUserMenu)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("unos novog kor");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.Clear();
+                        UserMenuDeleting();
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.Clear();
+                        UserMenuEditing();
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.Clear();
+                        UserMenuOverview();
+                        break;
+                    }
+                case 5:
+                    {
+                        userMenuRunning = false;
+                        Console.Clear();
+                        break;
+                    }
+                default:
+                    { 
+                        Console.WriteLine("Krivi unos, molimo pokusajte ponovo");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    }
+            }
+        }
+    }
+
+    static void UserMenuDeleting()
+    {
+        Console.WriteLine("a) Po id_u\nb) Po imenu i prezimenu\nc) Povratak");
+        var isInputCorrect = char.TryParse(Console.ReadLine(), out var inputForUserMenuDeleting);
+        switch(inputForUserMenuDeleting)
+        {
+            case 'a':
+                {
+                    Console.Clear();
+                    Console.WriteLine("po id-u");
+                    Console.ReadKey();
+                    break;
+                }
+            case 'b':
+                {
+                    Console.Clear();
+                    Console.WriteLine("po imenu i prezimenu");
+                    Console.ReadKey();
+                    break;
+                }
+            case 'c':
+                {
+                    isInputCorrect = false;
+                    Console.Clear();
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("Krivi unos, molimo pokusajte ponovo");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                }
+        }
+    }
+
+    static void UserMenuEditing()
+    {
+        Console.WriteLine("a) Po id_u\nb) Povratak");
+        var isInputCorrect = char.TryParse(Console.ReadLine(), out var inputForUserMenuEditing);
+        switch (inputForUserMenuEditing)
+        {
+            case 'a':
+                {
+                    Console.Clear();
+                    Console.WriteLine("po id-u");
+                    Console.ReadKey();
+                    break;
+                }
+            case 'b':
+                {
+                    isInputCorrect = false;
+                    Console.Clear();
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("Krivi unos, molimo pokusajte ponovo");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                }
+        } 
+    }
+
+    static void UserMenuOverview()
+    {
+        Console.WriteLine("a) Ispis svih korisnika abecedno po prezimenu\nb) Svih onih koji imaju više od 30 godina\nc) Svih onih koji imaju barem jedan račun u minusu\nd) Povratak");
+        var isInputCorrect = char.TryParse(Console.ReadLine(), out var inputForUserMenuOverview);
+        switch(inputForUserMenuOverview)
+        {
+            case 'a':
+                {
+                    Console.Clear();
+                    Console.WriteLine("po prezimenu");
+                    Console.ReadKey();
+                    break;
+                }
+            case 'b':
+                {
+                    Console.Clear();
+                    Console.WriteLine("stariji od 30");
+                    Console.ReadKey();
+                    break;
+                }
+            case 'c':
+                {
+                    Console.Clear();
+                    Console.WriteLine("minus");
+                    Console.ReadKey();
+                    break;
+                }
+            case 'd':
+                {
+                    isInputCorrect = false;
+                    Console.Clear();
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("Krivi unos, molimo pokusajte ponovo");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                }
+        }
+
+
+    }
+
+
+
 }
