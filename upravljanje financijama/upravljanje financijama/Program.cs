68b242127a2d4a1cfbbc0df8098f2511baf6a3dd
ViceElez <vice.elezovic12@gmail.com>
2024-11-10 16:34:02 +0100

    dodavanje main menu

---
+++ upravljanje financijama/upravljanje financijama/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        bool mainMenuRunning = true;
+        while (mainMenuRunning)
+        {
+            Console.WriteLine("1 - Korisnici\n2 - Racuni\n3 - Izlaz iz aplikacije");
+            var isInputCorrect = int.TryParse(Console.ReadLine(), out var inputForMainMenu);
+            switch (inputForMainMenu)
+            {
+                case 1:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("kor");
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("rac");
+                        break;
+                    }
+                case 3:
+                    {
+                        mainMenuRunning = false;
+                        break;
+                    }
+                default:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Krivi unos, molimo pokusajte ponovo");
+                        break;
+                    }
+            }
+        }
+
+    }
+}
